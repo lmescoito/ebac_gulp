cf7275a99a32de60efc72de8c53f8700fcf0754b
lmescoito <lmescoito@hotmail.com>
2023-07-25 18:17:35 -0300

    atualização de projetos:funcionalidades

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const cssmin = require ('gulp-cssmin')
+const rename = require ('gulp-rename')
+const uglify = require('gulp-uglify')
+const image = require('gulp-imagemin')
+
+
+function tarefasCSS(cb) {
+    
+    return gulp.src ([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './vendor/owl/css/owl.css',
+        './node_modules/font-awesome/css/font-awesome.css',
+        './vendor/jquery-ui/jquery-ui.css',
+        './src/css/style.css'
+
+    ])
+        .pipe(concat('styles.css'))
+        .pipe (cssmin())
+        .pipe (rename({suffix: '.min'})) // styles.min.css
+        .pipe(gulp.dest('./dist/css'))
+
+}
+
+function tarefasJS(){
+
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './vendor/jquery-ui/jquery-ui.js',
+        './src/js/custom.js'
+    
+    ])
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'})) // scripts.min.js
+        .pipe(gulp.dest('./dist/js'))
+
+
+}
+function tarefasImagem(){
+
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant:true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg:true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+
+    }     
+
+exports.styles = tarefasCSS
+exports.scripts = tarefasJS
+exports.images = tarefasImagem
+
